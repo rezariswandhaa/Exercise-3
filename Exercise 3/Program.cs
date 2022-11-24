ec282ecec60939dac03fddf97d046c28fc55ce36
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-11-24 16:36:57 +0700

    Add public addnote

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -25,7 +25,57 @@ namespace Exercise_3
             {
                 LAST = null;
             }
-           
+            public void addNode()
+            {
+                int rollNo;
+                string nm;
+                Console.Write("\nEnter the roll number of the student: ");
+                rollNo = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nEnter the name of the student: ");
+                nm = (Console.ReadLine());
+                Node newnode = new Node();
+                newnode.rollNumber = rollNo;
+                newnode.name = nm;
+
+                if (START == null || rollNo <= START.rollNumber)
+                {
+                    if ((START != null) && (rollNo == START.rollNumber))
+                    {
+                        Console.WriteLine("\nDuplicate roll numbers not allowed");
+                        return;
+                    }
+                    newnode.next = START;
+                    if (START != null)
+                        START.prev = newnode;
+                    newnode.prev = null;
+                    START = newnode;
+                    return;
+                }
+
+                Node previous, current;
+                for (current = previous = START; current != null &&
+                    rollNo >= current.rollNumber; previous = current, current =
+                    current.next)
+                {
+                    if (rollNo == current.rollNumber)
+                    {
+                        Console.WriteLine("\nDuplicate roll numbers not allowed");
+                        return;
+                    }
+                }
+
+                newnode.next = current;
+                newnode.prev = previous;
+
+                if (current == null)
+                {
+                    newnode.next = null;
+                    previous.next = newnode;
+                    return;
+                }
+                current.prev = newnode;
+                previous.next = newnode;
+            }
 
             public bool Search(int rollNO, ref Node previous, ref Node current)
             {
@@ -39,7 +89,7 @@ namespace Exercise_3
                 else
                     return (false);
             }
-            
+           
             public bool listEmpty()
             {
                 if (LAST == null)
