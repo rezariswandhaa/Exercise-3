cf539e88b0c94e54b7719bab92bc2d42f54d4b6e
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-11-24 16:08:08 +0700

    Add Main  and finish

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -67,13 +67,75 @@ namespace Exercise_3
                 else
                     Console.WriteLine("\nThe first record in the list is:\n\n " + LAST.next.rollNumber + "   " + LAST.next.name);
             }
-        }
-        
+            static void Main(string[] args)
+            {
+                CircularList obj = new CircularList();
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("\n Menu" +
+                            "\n 1. Add a record to the list" +
+                            "\n 2. Delete a record from the list" +
+                            "\n 3. View all records in the ascending order of roll numbers" +
+                            "\n 4. View all records in the descending order of roll numbers" +
+                            "\n 5. Search for a record in the list" +
+                            "\n 6. Exit \n" +
+                            "\n Enter your choice (1 - 6):");
+                        char ch = Convert.ToChar(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case '1':
+                                {
+                                    obj.traverse();
+                                }
+                                break;
+                            case '2':
+                                {
+                                    if (obj.listEmpty() == true)
+                                    {
+                                        Console.WriteLine("\nList is empty");
+                                        break;
+                                    }
+                                    Node prev, curr;
+                                    prev = curr = null;
+                                    Console.Write("\nEnter the rollnumber of the student Whose record is to be searched: ");
+                                    int num = Convert.ToInt32(Console.ReadLine());
+                                    if (obj.Search(num, ref prev, ref curr) == false)
+                                        Console.WriteLine("Record not found");
+                                    else
+                                    {
+                                        Console.WriteLine("\nRecord found");
+                                        Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                        Console.WriteLine("\nName: " + curr.name);
+                                    }
+                                }
+                                break;
+                            case '3':
+                                {
+                                    obj.firstNode();
+                                }
+                                break;
+                            case '4':
+                                return;
+                            default:
+                                {
+                                   Console.WriteLine("Invalid option");
+                                   break;
+                                }
+                                
+                            
+                                
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
 
+            }
 
-
-        static void Main(string[] args)
-        {
-        }
+        }       
     }
 }
